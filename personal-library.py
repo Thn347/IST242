fc234bbb823eed0e74805de765a077b9d8fae94f
Thn347 <thn5129@psu.edu>
2026-01-30 22:24:32 -0500

    Version 1.6 with "update" fucntion

--- personal-library.py
+++ personal-library.py
@@ -11,11 +11,12 @@ def show_menu():
     Display the main menu options to the user
     """
     print("\nPersonal Library Menu")
-    print("  1. Add Book")
-    print("  2. View Books")
-    print("  3. Search Book")
-    print("  4. Delete Book")
-    print("  5. Exit")
+    print("  1.Add Book")
+    print("  2.View Books")
+    print("  3.Search Book")
+    print("  4.Delete Book")
+    print("  5.Update Book")
+    print("  6.Exit")
 
 
 def add_book(library):
@@ -23,24 +24,21 @@ def add_book(library):
     Add a new book, its author and publication year to the library
     """
     title = input("Enter book title: ").strip().title()
-    if title in library:
-        print(f"\"{title}\" already existed")
-    else:
-        author = input("Enter the book's author: ").strip().title()
-        
-        while True:
-            year = input("Enter the publication year: ").strip()
-            if year.isdigit():
-                year_public = int(year)
-                if year_public >= 1000:
-                    break
-                else:
-                    print("Please put year that is >= 1000!")
+    
+    author = input("Enter the book's author: ").strip().title()
+    while True:
+        year = input("Enter the publication year: ").strip()
+        if year.isdigit():
+            year_public = int(year)
+            if year_public >= 1000:
+                break
             else:
-                print("Invalid input. Number only")
+                print("Please put year that is >= 1000!")
+        else:
+            print("Invalid input. Number only")
 
-        library[title] = {"author": author, "year": year_public}
-        print(f"\"{title}\" added successfully")
+    library[title] = {"author": author, "year": year_public}
+    print(f"\"{title}\" added successfully")
 
 
 def view_books(library):
@@ -51,9 +49,10 @@ def view_books(library):
         print("There are no available books")
         return
     
-    for title in sorted(library.keys()):
+    print("\nYour Books:") 
+    for i, title in enumerate(sorted(library.keys()), start=1):
         info = library[title]
-        print(f"{title} by {info['author']}, public in {info['year']}")
+        print(f"{i}. {title} - {info['author']}, public in {info['year']}")
    
 
 def search_book(library):
@@ -63,7 +62,7 @@ def search_book(library):
     title = input("Enter the book's title to search: ").strip().title()
     if title in library:
         info = library[title]
-        print(f"Found \"{title}\" by {info["author"]} - {info["year"]}")
+        print(f"Found \"{title}\" by {info['author']} - public in {info['year']}")
     else:
         print(f"\"{title}\" not found")
 
@@ -78,8 +77,75 @@ def delete_book(library):
         del library[title]
         print(f"\"{title}\" removed successfully.")
     else:
-        print(f"\"{title}\" not found!")
+        print(f"\"{title}\" not found! Try again: ")
 
+
+def update_book(library):
+    """
+    Updating existing book
+    """
+    title = input("Enter the book's title to update: ").strip().title()
+
+    if title not in library:
+        print(f"\"{title}\" not found!")
+        return
+    else:
+        info = library[title]
+        print(f"Current information: {title} by {info['author']}, public in {info['year']}")
+        print("Which one do you want to update?")
+        print("  1.Author")
+        print("  2.Publication year")
+        print("  3.Both")
+        print("  4.None")
+
+        while True:
+            updating = input("Choose your option: ").strip()
+
+            if updating == "1":
+                new_author = input("Enter the new update for author: ").strip().title()
+                info['author'] = new_author
+                print("Successfully updated")
+                break
+
+            elif updating == "2":
+                while True:
+                    new_year = input("Enter the new update for publication year: ").strip()
+                    if new_year.isdigit():
+                        new_public_year = int(new_year)
+                        if new_public_year >= 1000:
+                            info['year'] = new_public_year
+                            print("Successfully updated")
+                            break
+                        else:
+                            print("Please put year that is >= 1000!")
+                    else:
+                        print("Invalid input. Number only")
+                break
+
+            elif updating == "3":
+                new_author = input("Enter the new update for author: ").strip().title()
+                info['author'] = new_author
+                while True:
+                    new_year = input("Enter the new update for publication year: ").strip()
+                    if new_year.isdigit():
+                        new_public_year = int(new_year)
+                        if new_public_year >= 1000:
+                            info['year'] = new_public_year
+                            print("Successfully updated")
+                            break
+                        else:
+                            print("Please put year that is >= 1000!")
+                    else:
+                        print("Invalid input. Number only")
+                break
+
+            elif updating == "4":
+                print("No change made! See you again!")
+                return
+            
+            else:
+                print("Invalid choice - try again")
+            
 
 def main():
     """
@@ -100,7 +166,9 @@ def main():
         elif choice == "4":
             delete_book(library)
         elif choice == "5":
-            print("Goodbye")
+            update_book(library)
+        elif choice == "6":
+            print("Goodbye!")
             break
         else:
             print("Invalid choice - try again")
